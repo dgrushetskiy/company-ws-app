bdfc8cae604565659cd39b9396d360c81e2ef564
dgrushetskiy <dgrushetskiy@gmail.com>
2023-04-04 02:07:02 +0300

    add layers utils

---
+++ src/main/java/ru/gothmog/ws/company/core/utils/DatePatternUtils.java
@@ -0,0 +1,17 @@
+package ru.gothmog.ws.company.core.utils;
+
+public class DatePatternUtils {
+
+    public static final String DATE_FORMAT_T = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static final String DATE_FORMAT_KIZEN = "yyyy-MM-dd HH:mm:ss.SSSXXXXX";
+    public static final String DATETIME_RU_FORMAT = "dd.MM.yyyy HH:mm:ss";
+    public static final String DATE_FORMAT_RU = "dd.MM.yyyy";
+    public static final String DATE_FORMAT_TIME = "HH:mm:ss";
+    public static final String DATE_FORMAT_ENG = "yyyy-MM-dd";
+    public static final String POSTFIX_START_DAY = " 00:00:00";
+    public static final String POSTFIX_START_DAY_CLOCK_MIN = " 00:00";
+    public static final String POSTFIX_START_NOON = " 12:00:00";
+    public static final String POSTFIX_END_DAY = " 23:59:59";
+    public static final String POSTFIX_END_DAY_CLOCK_MIN = " 23:59";
+}
