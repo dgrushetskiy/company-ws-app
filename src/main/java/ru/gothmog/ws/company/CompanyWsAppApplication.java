b7bff1acd557a63df4d7bf7352d7cdf2c744bf84
dgrushetskiy <dgrushetskiy@gmail.com>
2023-04-04 02:02:22 +0300

    create project

---
+++ src/main/java/ru/gothmog/ws/company/CompanyWsAppApplication.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyWsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyWsAppApplication.class, args);
+	}
+
+}
